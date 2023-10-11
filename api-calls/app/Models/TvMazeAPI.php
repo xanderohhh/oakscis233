16ab921d097619eb2763dd0b1722983cd3e6656f
Xander Oaks <xanderoaks@gmail.com>
2023-10-11 16:09:05 -0700

    finished assignment

--- api-calls/app/Models/TvMazeAPI.php
+++ api-calls/app/Models/TvMazeAPI.php
@@ -1,20 +1,31 @@
 <?php
 
 namespace App\Models;
+
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Http;
 use App\Models\Episode;
 
-class TvMazeAPI {
-    public static function fetch($showNumber){
+class TvMazeAPI
+{
+    public static function fetch($showNumber)
+    {
         $episodesData = Http::get("https://api.tvmaze.com/shows/$showNumber/episodes")->json();
 
         $episodesCollection = collect($episodesData);
 
-    // return $episodesCollection;
+        // return $episodesCollection;
 
-        return $episodesCollection->map(function($show){
-            return new Episode($show['name'], $show['image']['original'], $show['season'], $show['number'], $show['summary']);
+        return $episodesCollection->map(function ($show) use ($showNumber) {
+            //using firstOrCreate here prevents the same show from being written to the database more than once
+            return Episode::firstOrCreate([
+                'name' => $show['name'],
+                'image' => $show['image']['original'],
+                'season' => $show['season'],
+                'episode' => $show['number'],
+                'summary' => strip_tags($show['summary']),
+                'show_number' => $showNumber
+            ]);
         });
 
 
--- api-calls/routes/web.php
+++ api-calls/routes/web.php
@@ -1,26 +1,25 @@
 <?php
 
-// namespace App\Models;
+namespace App\Models;
 use Illuminate\Support\Facades\Route;
-use App\Models\TvMazeAPI;
+use App\Models\Episode;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/episodes', function(){
+Route::get('/load-episodes', function(){
     $showNumber = intval(request()->query('showNumber'));
     $showNumber = $showNumber < 1 ? 1 : $showNumber;
     $episodes = TvMazeAPI::fetch($showNumber);
     return view('episodes/index', ['episodes' => $episodes]);
 });
+
+Route::get('/view-episodes', function(){
+    $showNumber = intval(request()->query('showNumber'));
+    $showNumber = $showNumber < 1 ? 1 : $showNumber;
+    $episodes = Episode::where('show_number', $showNumber)->get();
+    return view('episodes/index', ['episodes' => $episodes]);
+});
+
+
 
 Route::get('/', function () {
     return view('welcome');

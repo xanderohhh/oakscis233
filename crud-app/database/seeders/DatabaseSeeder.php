bbfdd4fa85fd63a0fe4dba47c077c020954afc51
Xander Oaks <xanderoaks@gmail.com>
2023-11-07 18:40:34 -0800

    added model factory

--- crud-app/database/seeders/DatabaseSeeder.php
+++ crud-app/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+ use App\Models\Product;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,12 +13,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // Product::factory(50)->create();
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $this->call(ProductSeeder::class);
+        // $this->call(ProductSeeder::class);
+        Product::query()->delete();
+        Product::factory()->count(50)->create();
+
     }
 }

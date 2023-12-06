d883d7e8dbb6c9f1af05123d11876837fd7e6683
Xander Oaks <xanderoaks@gmail.com>
2023-12-06 13:26:06 -0800

    assignment/products-api

--- crud-app/resources/views/products/edit.blade.php
+++ crud-app/resources/views/products/edit.blade.php
@@ -2,13 +2,7 @@
 @section('content')
     <h3>Edit Product</h3>
 
-    @if ($errors->any())
-    <div class="alert alert-danger">
-        @foreach ($errors->all() as $error)
-            <span>{{$error}}</span><br>
-        @endforeach
-    </div>
-    @endif
+
 
     <form class="mx-1" method="POST" action="{{ route('products.update', $product->id) }}">
         @csrf

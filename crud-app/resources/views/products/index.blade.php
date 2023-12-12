529cb8ff4d027c3fbadbed2ae73954d91e74077a
Xander Oaks <xanderoaks@gmail.com>
2023-12-11 16:41:57 -0800

    assignment/final

--- crud-app/resources/views/products/index.blade.php
+++ crud-app/resources/views/products/index.blade.php
@@ -8,42 +8,13 @@
             <div class="position-absolute top-5 end-0">{{ $products->links() }}</div>
         </div>
 
+        {{-- <div class="search">
+        <livewire:search-products />
+        </div> --}}
+
 
         <!-- Products List -->
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">Image</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Price</th>
-                    <th scope="col">Item Number</th>
-                    <th scope="col"></th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($products as $product)
-                    <tr>
-                        <td class=" align-middle " scope="row"><img src="{{ $product->image }}"
-                                class="rounded img-thumbnail" /></th>
-                        <td class=" align-middle "><a
-                                href="{{ route('products.show', $product->id) }}">{{ $product->name }}</a></td>
-                        <td class=" align-middle ">${{ $product->price }}</td>
-                        <td class=" align-middle ">{{ $product->item_number }}</td>
-                        <td class=" align-middle ">
-
-                            <a href="{{route('products.edit', $product->id)}}" role="button" class="btn btn-warning mb-1">Edit</a>
-
-                            <form action="{{ route('products.destroy', $product->id) }}" method="POST"
-                                onsubmit="return confirm('Are you sure you want to delete?')">
-                                @csrf
-                                @method('DELETE')
-                                <button class="btn btn-danger  mt-1" type="submit">Delete</button>
-                            </form>
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
+        <livewire:products-list />
 
         <!-- Pagination Links at the bottom -->
         <div class="d-flex justify-content-center">{{ $products->links() }}</div>
